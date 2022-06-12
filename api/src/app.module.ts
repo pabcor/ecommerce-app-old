694be35ffc14cb1ea9a3be8a21d7ed1e1196be4e
pabcor <99924607+pabcor@users.noreply.github.com>
2022-06-12 11:40:19 -0300

    Add mikro-orm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,3 +1,8 @@
+import {
+  ReflectMetadataProvider,
+  UnderscoreNamingStrategy,
+} from '@mikro-orm/core';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -11,6 +16,19 @@ import { ProductsModule } from './products/products.module';
 import { StocksModule } from './stocks/stocks.module';
 @Module({
   imports: [
+    MikroOrmModule.forRoot({
+      clientUrl: 'postgresql://root:1234@localhost/ecommerce',
+      debug: true,
+      entities: ['./dist/**/*.entity.js'],
+      entitiesTs: ['./src/**/*.entity.ts'],
+      metadataProvider: ReflectMetadataProvider,
+      migrations: {
+        path: './dist/database/migrations',
+        pathTs: './src/database/migrations',
+      },
+      namingStrategy: UnderscoreNamingStrategy,
+      tsNode: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       debug: true,
