276adc7dc2b6b37e52805662855a28d0aaae1687
pabcor <99924607+pabcor@users.noreply.github.com>
2022-06-12 11:32:41 -0300

    Add graphql

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,15 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { CustomersModule } from './customers/customers.module';
 import { CartsModule } from './carts/carts.module';
+import { CustomersModule } from './customers/customers.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { StocksModule } from './stocks/stocks.module';
-
 @Module({
-  imports: [AuthModule, CustomersModule, CartsModule, OrdersModule, ProductsModule, StocksModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      playground: true,
+    }),
+    AuthModule,
+    CustomersModule,
+    CartsModule,
+    OrdersModule,
+    ProductsModule,
+    StocksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
